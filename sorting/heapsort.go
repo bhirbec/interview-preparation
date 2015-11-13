1ac3d199b1a357d7ec80c80ac7164c3c7818a16e
Benoit Hirbec <benoit@gardentechno.com>
2015-11-12 23:20:11 -0800

    Add heapsort

---
+++ sorting/heapsort.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Heap []int
+
+func (h *Heap) Insert(value int) {
+	*h = append(*h, value)
+	a := *h
+
+	var child, parent int
+	child = len(a) - 1
+
+	for child > 0 {
+		parent = child / 2
+		if child&1 == 0 {
+			parent--
+		}
+
+		if a[child] < a[parent] {
+			a[child], a[parent] = a[parent], a[child]
+			child = parent
+		} else {
+			break
+		}
+	}
+}
+
+func (h *Heap) Extract() (int, error) {
+	var parent, child int
+
+	a := *h
+	if len(a) == 0 {
+		return 0, fmt.Errorf("Heap error: cannot extract value from an empty heap")
+	}
+
+	parent = 0
+	last := len(a) - 1
+	root := a[parent]
+	a[parent] = a[last]
+	*h = a[:last]
+	last--
+
+	for {
+		left := 2*(parent+1) - 1
+		if left > last {
+			break
+		}
+
+		right := left + 1
+		if right <= last && a[right] < a[left] {
+			child = right
+		} else {
+			child = left
+		}
+
+		if a[parent] > a[child] {
+			a[parent], a[child] = a[child], a[parent]
+			parent = child
+		} else {
+			break
+		}
+	}
+
+	return root, nil
+}
+
+func heapsort(array []int) []int {
+	n := len(array)
+
+	// heapify
+	heap := &Heap{}
+	for i := 0; i < n; i++ {
+		heap.Insert(array[i])
+	}
+
+	// sort
+	output := make([]int, n)
+	for i := 0; i < n; i++ {
+		output[i], _ = heap.Extract()
+	}
+
+	return output
+}
+
+func main() {
+	array := []int{2, 4, 6, 2, 2, -2, 2, 3, 6, -4, 5, 6, 8, 5, 7, 8}
+	array = heapsort(array)
+	fmt.Println(array)
+}
