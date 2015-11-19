93531a170e2c4a439a1a6d7dee9b8460df1ab613
Benoit Hirbec <benoit@gardentechno.com>
2015-11-18 23:15:58 -0800

    Add a function that make a BTS from a sorted array

--- tree/tree.go
+++ tree/tree.go
@@ -1,8 +1,35 @@
 package tree
 
+import (
+	"fmt"
+
+	"github.com/bhirbec/Algorithms-And-Data-Structures/sort"
+)
+
 type Node struct {
 	left, right *Node
 	value       int
+}
+
+// MakeBTS make a BTS from the given sorted array. It returns an
+// error if the given array isn't sorted in ascending order.
+func MakeBTS(array []int) (*Node, error) {
+	if !sort.IsSorted(array) {
+		return nil, fmt.Errorf("Tree error: input array must be sorted.")
+	}
+	return makeBTS(array), nil
+}
+
+func makeBTS(array []int) *Node {
+	n := len(array)
+	if n < 1 {
+		return nil
+	}
+
+	l := n / 2
+	left := makeBTS(array[:l])
+	right := makeBTS(array[l+1:])
+	return &Node{left, right, array[l]}
 }
 
 // IsBSTNaive returns true is the given tree is a BST. Otherwise returns false.
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -4,6 +4,15 @@ import (
 	"testing"
 )
 
+func TestMakeBTS(t *testing.T) {
+	array := []int{1, 3, 4, 6, 7, 8, 10, 12, 13, 15, 16, 17}
+	root, _ := MakeBTS(array)
+
+	if !IsBSTNaive(root) {
+		t.Error("MakeBTS failed.")
+	}
+}
+
 func TestIsBSTNaive(t *testing.T) {
 	tree := &Node{
 		value: 12,
