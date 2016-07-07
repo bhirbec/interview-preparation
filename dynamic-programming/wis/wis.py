20f695cb7da2a7d3005554d6cf0739ee87c68be9
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-07-06 22:37:20 -0700

    WIS - improve recursive algorithm by reconstructing the solution

--- dynamic-programming/wis/wis.py
+++ dynamic-programming/wis/wis.py
@@ -1,33 +1,19 @@
 
-
 def recursive(graph):
     n = len(graph)
-    cache = [None]*n
+    cache = [None]*(n+1)
+    cache[0], cache[1] = 0, graph[0]
 
     def _f(i):
-        if i < 0:
-            return 0, []
-
-        w = graph[i]
-        if i == 0:
-            return graph[i], [w]
-        elif i in cache:
-            return cache[i]
-
-        S2, IS2 = _f(i-2)
-        S1, IS1 = _f(i-1)
-        S2 += w
-
-        if S1 > S2:
-            cache[i] = S1, IS1
-        else:
-            # TODO: remove list concat. It makes this algorithm O(n**2)
-            cache[i] = S2, IS2 + [w]
-
+        if cache[i] is None:
+            w = graph[i-1]
+            S2 = _f(i-2) + w
+            S1 = _f(i-1)
+            cache[i] = S1 if S1 > S2 else S2
         return cache[i]
 
-    _, IS =  _f(n-1)
-    return IS
+    _f(n)
+    return _reconstruct_solution(n, cache)
 
 def bottom_up(graph):
     n = len(graph)
@@ -55,9 +41,7 @@ def _reconstruct_solution(n, cache):
     output.reverse()
     return output
 
-    return cache[n-1][1]
-
 if __name__ == '__main__':
-    graph = [17, 14, 5, 4, 8, 12, 1, 34, 108, 22]
+    graph = [17, 14, 5, 4, 82, 12, 1, 34, 1080, 222]
     print recursive(graph)
     print bottom_up(graph)
