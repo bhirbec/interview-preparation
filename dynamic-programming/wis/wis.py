2fcb3a129e53bbf8f3f431fefa35d4ce797f2db7
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-07-06 21:35:33 -0700

    Add bottom-up algorithm for WIS problem

--- dynamic-programming/wis/wis.py
+++ dynamic-programming/wis/wis.py
@@ -21,14 +21,39 @@ def recursive(graph):
         if S1 > S2:
             cache[i] = S1, IS1
         else:
-            IS2.append(w)
-            cache[i] = S2, IS2
+            # TODO: remove list concat. It makes this algorithm O(n**2)
+            cache[i] = S2, IS2 + [w]
 
         return cache[i]
 
     _, IS =  _f(n-1)
     return IS
 
+def bottom_up(graph):
+    n = len(graph)
+    cache = [0]*n
+    cache[0] = graph[0], [graph[0]]
+
+    for i in range(1, n):
+        w = graph[i]
+
+        if i-2 < 0:
+            S2, IS2 = 0, []
+        else:
+            S2, IS2 = cache[i-2]
+
+        S1, IS1 = cache[i-1]
+        S2 += w
+
+        if S1 > S2:
+            cache[i] = S1, IS1
+        else:
+            # TODO: remove list concat. It makes this algorithm O(n**2)
+            cache[i] = S2, IS2 + [w]
+
+    return cache[n-1][1]
+
 if __name__ == '__main__':
-    graph = [17, 4, 5, 4, 8, 12, 7]
+    graph = [17, 14, 5, 4, 8, 12, 1, 34, 108, 22]
     print recursive(graph)
+    print bottom_up(graph)
