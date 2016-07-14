d380e60e827bd0d774f3a674abfa290b32efdb7c
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-07-13 22:37:33 -0700

    Fibonacci - replace tabs with spaces

--- dynamic-programming/fibonacci/fibonacci.py
+++ dynamic-programming/fibonacci/fibonacci.py
@@ -1,28 +1,28 @@
 
 
 def recursive_fib(n):
-	memo = {}
+    memo = {}
 
-	def _fib(n):
-		if n <= 2:
-			return 1
-		elif n in memo:
-			f = memo[n]
-		else:
-			f = _fib(n-2) + _fib(n-1)
-			memo[n] = f
-		return f
+    def _fib(n):
+        if n <= 2:
+            return 1
+        elif n in memo:
+            f = memo[n]
+        else:
+            f = _fib(n-2) + _fib(n-1)
+            memo[n] = f
+        return f
 
-	return _fib(n)
+    return _fib(n)
 
 def bottom_up_fib(n):
-	memo = {}
-	for k in range(1, n+1):
-		if k <= 2:
-			memo[k] = 1
-		else:
-			memo[k] = memo[k-2] + memo[k-1]
-	return memo[n]
+    memo = {}
+    for k in range(1, n+1):
+        if k <= 2:
+            memo[k] = 1
+        else:
+            memo[k] = memo[k-2] + memo[k-1]
+    return memo[n]
 
 print recursive_fib(10)
 print bottom_up_fib(10)
