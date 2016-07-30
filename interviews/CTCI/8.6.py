c6a20ec1e03abe343c5102604354db0712d26a5b
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-07-30 14:12:46 -0700

    Add solution for CTCI problem 8.6

---
+++ interviews/CTCI/8.6.py
@@ -0,0 +1,29 @@
+# Notes:
+# I really struggled on this one and didn't think about a recursive algorithm. I
+# had to read the solution to understand that we could express the problem as
+# subproblems of smaller size.
+
+# Time: O(2^n) (drawing the tree of subproblems helped)
+# Space: O(log(n))
+
+def main():
+	n = 5
+	origin = range(1, n+1)
+	buff = []
+	dest = []
+	move_disks(n, origin, buff, dest)
+	print dest
+
+def move_disks(n, origin, buff, dest):
+    if n == 1:
+    	move_top(origin, dest)
+    	return
+
+    move_disks(n-1, origin, dest, buff)
+    move_top(origin, dest)
+    move_disks(n-1, buff, origin, dest)
+
+def move_top(origin, dest):
+	dest.append(origin.pop())
+
+main()
