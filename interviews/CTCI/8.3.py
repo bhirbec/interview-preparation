dfa2bfd78ee29ecc1117da193a1c2ebbb44cbe33
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-07-31 18:28:03 -0700

    Add solution for CTCI problem 8.3

---
+++ interviews/CTCI/8.3.py
@@ -0,0 +1,26 @@
+
+def main():
+    array = [-1, 1, 3, 5, 7]
+    print magix_index_distinct_array(array)
+
+    array = [-1, 0, 3, 5, 7]
+    print magix_index_distinct_array(array)
+
+def magix_index_distinct_array(array):
+    n = len(array)
+
+    def _f(i, j):
+        if j < i:
+            return -1
+
+        index = int((i + j) / 2)
+        if array[index] == index:
+            return index
+        elif array[index] > index:
+            return _f(i, index-1)
+        else:
+            return _f(index+1, j)
+
+    return _f(0, n-1)
+
+main()
