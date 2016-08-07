db715530487a11b4a9b3a599f75ad4df50c9d7c0
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-08-06 22:51:38 -0700

    LIS - convert indentation to spaces

--- dynamic-programming/LIS/LIS.py
+++ dynamic-programming/LIS/LIS.py
@@ -9,9 +9,9 @@
 # *************
 
 # for i=N; i>0l i-- {
-# 	find all subsequence of D with length i
-# 	if there is one subsequence:
-# 		break
+#   find all subsequence of D with length i
+#   if there is one subsequence:
+#       break
 # }
 
 # Time complexity: O(2^N)
@@ -37,50 +37,50 @@
 # Time Complexity: O(n^2)
 
 def recursive(array):
-	def _f(i):
-		if i < 0:
-			return 0
+    def _f(i):
+        if i < 0:
+            return 0
 
-		if cache[i] > 0:
-			return cache[i]
+        if cache[i] > 0:
+            return cache[i]
 
-		length = 1
-		for j in xrange(i):
-			s = _f(j)
-			if array[i] >= array[j] and s + 1 > length:
-				length = s + 1
+        length = 1
+        for j in xrange(i):
+            s = _f(j)
+            if array[i] >= array[j] and s + 1 > length:
+                length = s + 1
 
-		cache[i] = length
-		return length
+        cache[i] = length
+        return length
 
-	n = len(array)
-	cache = [0]*n
-	m = _f(n-1)
-	return reconstruct(m, n, cache, array)
+    n = len(array)
+    cache = [0]*n
+    m = _f(n-1)
+    return reconstruct(m, n, cache, array)
 
 def bottom_up(array):
-	n = len(array)
-	cache = [1] * n
+    n = len(array)
+    cache = [1] * n
 
-	for i in xrange(n):
-		for j in xrange(i):
-			if array[i] >= array[j] and cache[j] + 1 > cache[i]:
-				cache[i] = cache[j] + 1
+    for i in xrange(n):
+        for j in xrange(i):
+            if array[i] >= array[j] and cache[j] + 1 > cache[i]:
+                cache[i] = cache[j] + 1
 
-	m = max(cache)
-	return reconstruct(m, n, cache, array)
+    m = max(cache)
+    return reconstruct(m, n, cache, array)
 
 def reconstruct(m, n, cache, array):
-	i = n - 1
-	output = []
+    i = n - 1
+    output = []
 
-	while i > 0:
-		if cache[i] == m:
-			output.append(array[i])
-			m -= 1
-		i -= 1
+    while i > 0:
+        if cache[i] == m:
+            output.append(array[i])
+            m -= 1
+        i -= 1
 
-	return list(reversed(output))
+    return list(reversed(output))
 
 array = [1, 2, 0, -8, 0, 6, 7, 3, 3, 89]
 print recursive(array)
