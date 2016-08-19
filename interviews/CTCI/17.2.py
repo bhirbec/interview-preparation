5234f5577bf162080e76b29376b78d7cf8703677
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-08-18 23:27:55 -0700

    Add solution for CTCI 17.2

---
+++ interviews/CTCI/17.2.py
@@ -0,0 +1,22 @@
+import random
+
+
+def main():
+	print shuffle(52)
+
+def shuffle(n):
+	if n == 0:
+		return []
+
+	# shuffle n-1
+	result = shuffle(n-1)
+	# pick a pos between 0 and n-1
+	i = int(random.uniform(0, n))
+	# append the nth number
+	result.append(n)
+	# swap nth with ith
+	result[i], result[n-1] = result[n-1], result[i]
+	return result
+
+if __name__ == '__main__':
+	main()
