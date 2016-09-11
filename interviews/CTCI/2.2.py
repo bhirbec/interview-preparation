c49667946c4725a8f654a2de0ae22cb1c910c496
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-09-10 23:58:51 -0700

    Convert indentation to spaces

--- interviews/CTCI/2.2.py
+++ interviews/CTCI/2.2.py
@@ -1,49 +1,49 @@
 
 class Node():
-	def __init__(self, value, next_node=None):
-		self.value = value
-		self.next_node = next_node
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
 def main():
-	head = Node(12, Node(10, Node(3, Node(1, Node(156, Node(43))))))
-	print find_kth_from_tail(head, 2)
-	print find_kth_from_tail_runner(head, 2)
+    head = Node(12, Node(10, Node(3, Node(1, Node(156, Node(43))))))
+    print find_kth_from_tail(head, 2)
+    print find_kth_from_tail_runner(head, 2)
 
-	print find_kth_from_tail(None, 2)
-	print find_kth_from_tail_runner(None, 2)
+    print find_kth_from_tail(None, 2)
+    print find_kth_from_tail_runner(None, 2)
 
 
 def find_kth_from_tail_runner(n, k):
-	runner = n
-	for _ in xrange(k):
-		if runner is None:
-			return None
-		runner = runner.next_node
+    runner = n
+    for _ in xrange(k):
+        if runner is None:
+            return None
+        runner = runner.next_node
 
-	while runner.next_node:
-		n = n.next_node
-		runner = runner.next_node
+    while runner.next_node:
+        n = n.next_node
+        runner = runner.next_node
 
-	return n.value
+    return n.value
 
 def find_kth_from_tail(n, k):
-	def _f(n, k):
-		if n is None:
-			return None, -1
+    def _f(n, k):
+        if n is None:
+            return None, -1
 
-		k_node, l = _f(n.next_node, k)
-		if k_node is not None:
-			return k_node, l
+        k_node, l = _f(n.next_node, k)
+        if k_node is not None:
+            return k_node, l
 
-		l += 1
-		if l == k:
-			return n, l
+        l += 1
+        if l == k:
+            return n, l
 
-		return None, l
+        return None, l
 
-	n, l = _f(n, k)
-	if n is None:
-		return None
-	return n.value
+    n, l = _f(n, k)
+    if n is None:
+        return None
+    return n.value
 
 main()
--- interviews/CTCI/2.4.py
+++ interviews/CTCI/2.4.py
@@ -1,41 +1,41 @@
 
 class Node():
-	def __init__(self, value, next_node=None):
-		self.value = value
-		self.next_node = next_node
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
 def main():
-	n = Node(70, Node(134, Node(3, Node(1, Node(10, Node(9))))))
+    n = Node(70, Node(134, Node(3, Node(1, Node(10, Node(9))))))
 
-	n = partition(n, 10)
-	while n:
-		print n.value
-		n = n.next_node
+    n = partition(n, 10)
+    while n:
+        print n.value
+        n = n.next_node
 
 def partition(n, k):
-	left, right = None, None
-	while n:
-		next_node = n.next_node
+    left, right = None, None
+    while n:
+        next_node = n.next_node
 
-		if n.value >= k:
-			n.next_node = right
-			right = n
-		else:
-			n.next_node = left
-			left = n
+        if n.value >= k:
+            n.next_node = right
+            right = n
+        else:
+            n.next_node = left
+            left = n
 
-		n = next_node
+        n = next_node
 
-	if right is None:
-		return left
-	elif left is None:
-		return right
+    if right is None:
+        return left
+    elif left is None:
+        return right
 
-	n = left
-	while n.next_node:
-		n = n.next_node
+    n = left
+    while n.next_node:
+        n = n.next_node
 
-	n.next_node = right
-	return left
+    n.next_node = right
+    return left
 
 main()
