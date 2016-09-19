05b05f239c4d19bbca45c64f6e1a60fcc2df3a01
Benoit Hirbec <benoit.hirbec@gmail.com>
2016-09-18 21:47:03 -0700

    Improve LIS solution

--- dynamic-programming/LIS/LIS.py
+++ dynamic-programming/LIS/LIS.py
@@ -1,46 +1,15 @@
-# Problem:
-# For a vector of size N, find its LIS
 
-# Example:
-# D = 3, 2, 6, 4, 5 1
-# LIS = 2, 4, 5
+def main():
+    array = [1, 2, 0, -8, 0, 6, 7, 3, 3, -89]
+    print recursive(array)
+    print bottom_up(array)
 
-# 1.Naive Algo:
-# *************
-
-# for i=N; i>0l i-- {
-#   find all subsequence of D with length i
-#   if there is one subsequence:
-#       break
-# }
-
-# Time complexity: O(2^N)
-
-# Dynamic Programming Algorithm
-# *****************************
-
-# subproblem: we define a vector L such that:
-# - L[i] = LIS of D that end with D[i]
-# - L[0] = {D[0]}
-
-# D = 3, 2, 6, 4, 5 1
-# L[0] = {3}
-# L[1] = {2}
-# L[2] = {2, 6}
-# L[3] = {2, 4}
-# L[4] = {2, 4, 5}
-# L[5] = {1}
-
-# General formula:
-# L[i] = MAX(L[i] | i<j, D[j] < D[i]) + D[i]
-
-# Time Complexity: O(n^2)
 
 def recursive(array):
-    def _f(i):
-        if i < 0:
-            return 0
+    n = len(array)
+    cache = [0] * n
 
+    def _f(i):
         if cache[i] > 0:
             return cache[i]
 
@@ -53,10 +22,9 @@ def recursive(array):
         cache[i] = length
         return length
 
-    n = len(array)
-    cache = [0]*n
-    m = _f(n-1)
-    return reconstruct(m, n, cache, array)
+    _f(n - 1)
+    return reconstruct(array, cache, n)
+
 
 def bottom_up(array):
     n = len(array)
@@ -67,10 +35,11 @@ def bottom_up(array):
             if array[i] >= array[j] and cache[j] + 1 > cache[i]:
                 cache[i] = cache[j] + 1
 
-    m = max(cache)
-    return reconstruct(m, n, cache, array)
+    return reconstruct(array, cache, n)
 
-def reconstruct(m, n, cache, array):
+
+def reconstruct(array, cache, n):
+    m = max(cache)
     i = n - 1
     output = []
 
@@ -82,6 +51,4 @@ def reconstruct(m, n, cache, array):
 
     return list(reversed(output))
 
-array = [1, 2, 0, -8, 0, 6, 7, 3, 3, 89]
-print recursive(array)
-print bottom_up(array)
+main()
---
+++ dynamic-programming/LIS/README.md
@@ -0,0 +1,45 @@
+# Longuest Increasing Subsequence
+
+Problem:
+For a vector of size N, find its LIS
+
+Example:
+```
+D = 3, 2, 6, 4, 5 1
+LIS = 2, 4, 5
+```
+
+## Naive Algo:
+
+```
+for i = N; i > 0; i-- {
+  find all subsequence of D with length i
+  if there is one subsequence:
+      break
+}
+```
+
+Time complexity: O(2^N)
+
+## Dynamic Programming Algorithm
+
+subproblem: we define a vector L such that:
+- L[i] = LIS of D that end with D[i]
+- L[0] = {D[0]}
+
+```
+D = 3, 2, 6, 4, 5 1
+L[0] = {3}
+L[1] = {2}
+L[2] = {2, 6}
+L[3] = {2, 4}
+L[4] = {2, 4, 5}
+L[5] = {1}
+```
+
+General formula:
+```
+L[i] = MAX(L[i] | i<j, D[j] < D[i]) + D[i]
+```
+
+Time Complexity: O(n^2)
